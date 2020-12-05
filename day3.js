26ee5778e281eaa006801c32185b2e8044d07906
Jaredt <jdhultink@gmail.com>
2020-12-05 16:16:52 -0500

    day 3

---
+++ day3.js
@@ -0,0 +1,58 @@
+const { transcode } = require('buffer');
+const fs = require('fs')
+
+console.log(process.cwd())
+
+const raw = fs.readFileSync('input.txt').toString()
+let lines = raw.split('\r\n')
+
+part2();
+
+function part1()
+{    
+    let hits = 0;
+    let x = 0;
+    
+    for(line of lines)
+    {
+        let c = line[x % line.length]
+        if (c == '#')
+        {
+            hits++;
+        }
+
+        x += 3;
+    }
+
+    console.log(hits)
+}
+
+function part2()
+{    
+    let res = traverse(1, 1)
+            * traverse(3, 1)
+            * traverse(5, 1)
+            * traverse(7, 1)
+            * traverse(1, 2)
+     
+    console.log(res)
+}
+
+function traverse(xDelta, yDelta)
+{
+    let hits = 0;
+    let x = 0;
+
+    for(let y = 0; y < lines.length; y += yDelta)
+    {
+        let c = lines[y][x % lines[0].length]
+        if (c == '#')
+        {
+            hits++;
+        }
+
+        x += xDelta;
+    }
+    
+    return hits
+}
